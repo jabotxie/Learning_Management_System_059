2bf5d41b1e32698d24f5ddae13af6f5609332c7f
jabotxie <jiaxiejason@gmail.com>
2022-04-10 02:17:23 -0400

    Renamed and create local data functions

---
+++ src/DataManager.java
@@ -0,0 +1,157 @@
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * A class that helps to read and store the user information in a format of
+ * <p>
+ * ************************************
+ * username
+ * password
+ * User Type(T for Teacher, S for Student)
+ * ************************************
+ */
+
+public class DataManager {
+
+    public static ArrayList<User> users;
+    public static final Object usersSync = new Object();
+    public static ArrayList<Course> courses;
+    public static final Object coursesSync = new Object();
+    private static boolean isDataInitializedFromFile = false;
+    public static String usersInfoFileName = "UserInfo.txt";
+    public static String coursesInfoFileName = "CoursesInfo.txt";
+
+    public static void initData() {
+        synchronized (coursesSync) {
+            synchronized (usersSync) {
+                if (!isDataInitializedFromFile) {
+                    try {
+                        users = getUsersFromFile();
+                    } catch (FileNotFoundException e) {//No users in database
+                        users = new ArrayList<>();
+                    }
+                    try {
+                        courses = getCoursesFromFile();
+                    } catch (IOException | ClassNotFoundException e) {
+                        courses = new ArrayList<>();
+                    }
+                    isDataInitializedFromFile = true;
+                }
+            }
+        }
+    }
+
+    public static void saveData() {
+        saveUserInfo();
+        saveCoursesToFile();
+    }
+
+    private static ArrayList<Course> getCoursesFromFile() throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(coursesInfoFileName));
+        DataManager dataManager = (DataManager) objectInputStream.readObject();
+        return dataManager.getCourses();
+    }
+
+    private static void saveCoursesToFile(){
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(coursesInfoFileName));
+            objectOutputStream.writeObject(courses);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static ArrayList<User> getUsersFromFile() throws FileNotFoundException {
+
+        BufferedReader bf = new BufferedReader(new FileReader(usersInfoFileName));
+        ArrayList<String> lines = new ArrayList<>();
+        ArrayList<User> users = new ArrayList<>();
+
+        try {
+            String line = bf.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = bf.readLine();
+            }
+            bf.close();
+
+            for (int i = 0; i < lines.size(); i += 3) {
+                String username = lines.get(i);
+                String password = lines.get(i + 1);
+                String userType = lines.get(i + 2);
+                Class<? extends User> t = userType.equals("T") ? Teacher.class : Student.class;
+                if (t == Teacher.class) {
+                    users.add(new Teacher(username, password));
+                } else {
+                    users.add(new Student(username, password));
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public static void saveUserInfo() {
+        synchronized (usersSync) {
+
+            StringBuilder sb = new StringBuilder();
+            for (User user : users) {
+                sb.append(user.getUsername()).append('\n');
+                sb.append(user.getPassword()).append('\n');
+                sb.append(user.getClass() == Teacher.class ? "T" : "S").append('\n');
+            }
+            try {
+                BufferedWriter bw = new BufferedWriter(new FileWriter(usersInfoFileName));
+                bw.write(sb.toString());
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void login(String username, String password) throws LoginUnsuccessfulException {
+        synchronized (usersSync) {
+            int i = users.indexOf(new Student(username, password));
+            try {
+                if (users.get(i).getPassword().equals(password)) {
+                    UserActivities.currentUser = users.get(i);
+                } else {
+                    throw new LoginUnsuccessfulException();
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new LoginUnsuccessfulException();
+            }
+        }
+
+    }
+
+    public static void createAccount(Class<? extends User> c, String username, String password)
+            throws UsernameAlreadyTakenException {
+        synchronized (usersSync) {
+            if (users.contains(new Student(username, password))) {
+                throw new UsernameAlreadyTakenException();
+            } else {
+                if (c == Teacher.class) {
+                    users.add(new Teacher(username, password));
+                    UserActivities.currentUser = new Teacher(username, password);
+                } else {
+                    users.add(new Student(username, password));
+                    UserActivities.currentUser = new Student(username, password);
+                }
+                saveUserInfo();
+            }
+        }
+
+    }
+
+    private ArrayList<Course> getCourses() {
+        return courses;
+    }
+
+}
