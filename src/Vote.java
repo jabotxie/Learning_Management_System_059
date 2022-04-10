24b8683b0684798e991393952c6243dc1e7d2c73
jabotxie <jiaxiejason@gmail.com>
2022-04-10 18:56:12 -0400

    Vote class created

---
+++ src/Vote.java
@@ -0,0 +1,11 @@
+public class Vote {
+    Student student;
+
+    public Vote(Student student) {
+        this.student = student;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+}
