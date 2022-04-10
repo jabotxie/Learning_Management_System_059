3b647d99fda969fc21b6ab8fbd9bd6cf2b3a3315
jabotxie <jiaxiejason@gmail.com>
2022-04-10 02:18:55 -0400

    update the exception description

--- src/NoPermissionException.java
+++ src/NoPermissionException.java
@@ -1,6 +1,6 @@
 public class NoPermissionException extends Exception{
     @Override
     public String toString() {
-        return "You don't have permission to delete the post.";
+        return "You don't have permission to proceed the action.";
     }
 }
