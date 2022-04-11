97ffdcc0bc72b4d53e2221c76dede5258fa9acc3
Knittala <67932366+Knittala@users.noreply.github.com>
2022-04-11 15:38:40 -0400

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,18 +1,25 @@
 /**
  * Project 4 -- Learning Management System
  * <p>
- * A student class that represents a student. It is extended from User
+ * A student class that represents a student. It is extended from User. The class handles all functionality students can perform such as replying, voting, and viewing
+ their course dashboard.
+
  *
- * <p>Purdue University -- CS18000 -- Spring 2021</p>
+ * <p>Purdue University -- CS18000 -- Spring 2022</p>
  *
- * @author Jia Xie, Shreyash, Kundana, Garv
+ * @author Kundana Nittala, Jia Xie
  * @version April 11, 2022
  */
 public class Student extends User {
+    
+    //Here the Student constructor is declared with the username and password parameters. The username and password variables are brought into the class
+    //through inheritance and the utilization of super.
 
     public Student(String username, String password) {
         super(username, password);
     }
+    
+    //Here the addPost method is utilized for parameters forum and post. Adding a post to the forum is synchronized.
 
     @Override
     public void addPost(DiscussionForum forum, DiscussionPost post) {
@@ -21,10 +28,13 @@ public class Student extends User {
         }
     }
 
+     //Here the deletePost method is utilized for parameters forum and post. Throws the NoPermissionException.
+    
     @Override
     public void deletePost(DiscussionForum forum, DiscussionPost post) throws NoPermissionException {
         throw new NoPermissionException();
     }
+    
 
     @Override
     public void addReply(DiscussionPost post, DiscussionPost reply) {
@@ -33,6 +43,7 @@ public class Student extends User {
         }
     }
 
+    
     @Override
     public void createForum(Course course, String topic) throws NoPermissionException {
         throw new NoPermissionException();
