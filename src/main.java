13215b923b2ab07e0955c72bbc7d0435d787de5c
jabotxie <jiaxiejason@gmail.com>
2022-04-10 23:30:17 -0400

    Unused test class deleted

--- src/main.java
+++ src/main.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 
 public class main {
     public static void main(String[] args) {
-        User.initForum();
-        SwingUtilities.invokeLater(new UserActivities());
-        User.saveForum();
+        DataManager.initData();
+//        SwingUtilities.invokeLater(new UserActivities());
+        DataManager.saveData();
     }
 }
