d4b024fd70dc178251a07064916438bc118bf0b0
mad-garv <103150358+mad-garv@users.noreply.github.com>
2022-04-11 15:42:22 -0400

    javadoc edits

--- src/AlreadyVotedException.java
+++ src/AlreadyVotedException.java
@@ -3,7 +3,7 @@
  * <p>
  * Throw when a student tries to vote more than once
  *
- * <p>Purdue University -- CS18000 -- Spring 2021</p>
+ * <p>Purdue University -- CS18000 -- Spring 2022</p>
  *
  * @author Jia Xie, Shreyash, Kundana, Garv
  * @version April 11, 2022
